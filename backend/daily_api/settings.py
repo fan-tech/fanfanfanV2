723581811631fb0f2dd0915c4a957004e69f4edb
fan-tech <fan.n.o.to.39@gmail.com>
2022-10-02 18:57:24 +0900

    消してしまったところを戻した

--- backend/daily_api/settings.py
+++ backend/daily_api/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-from pathlib import
+from pathlib import Path
 import os
 import environ
 
